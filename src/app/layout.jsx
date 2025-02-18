e4f2e03c4dd1308d35905a08d7855c4c5373206a
Mathys <pieterm.basson@gmail.com>
2025-02-18 18:36:07 +0200

    Add custom layout and init fonts

---
+++ src/app/layout.jsx
@@ -0,0 +1,53 @@
+import { Cormorant_Garamond } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const cg_init = Cormorant_Garamond({
+    subsets: ["latin"],
+    weight: ["400", "500", "600"],
+    variable: "--font-cormorant-garamond",
+});
+
+const gtSuper = localFont({
+    src: [
+        {
+            path: "../../public/fonts/GT-Super-Display-Light.woff",
+            weight: "300",
+            style: "normal",
+        },
+        {
+            path: "../../public/fonts/GT-Super-Display-Light-Italic.woff",
+            weight: "300",
+            style: "italic",
+        },
+        {
+            path: "../../public/fonts/GT-Super-Display-Bold.woff",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../../public/fonts/GT-Super-Display-Bold-Italic.woff",
+            weight: "700",
+            style: "italic",
+        },
+    ],
+    variable: "--font-gt-super", // Optional: Expose as a CSS variable
+    display: "swap", // Ensures better performance
+});
+
+export const metadata = {
+    title: "George Marine",
+    description: "Ensuring smooth sailing with 30 years of expertise",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body
+                className={`${cg_init.variable} ${gtSuper.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
